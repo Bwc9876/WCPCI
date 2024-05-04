091522ce28cd6d0da086238fd8177168f9844d5d
Ben C <bwc9876@gmail.com>
2024-05-04 14:30:47 -0400

    Fix rust-analyzer in dev shell

--- flake.nix
+++ flake.nix
@@ -14,16 +14,20 @@
       };
 
     gitRev = self.shortRev or self.dirtyShortRev or "";
-    rawVersion = (builtins.importTOML ./Cargo.toml).version;
+    # TODO: Change back once Spoon fixes this
+    rawVersion = "0.1.0"; # (builtins.importTOML ./Cargo.toml).version;
     version = rawVersion + "-" + gitRev;
     packages = pkgs: rec {
       backend = pkgs.callPackage ./nix/backend.nix {inherit version gitRev;};
       frontend = pkgs.callPackage ./nix/frontend.nix {inherit version;};
       wrapper = pkgs.callPackage ./nix/wrapper.nix {inherit version backend frontend rocket_config;};
-      rocket_config = pkgs.callPackage ./nix/rocket_config.nix {openjdk = pkgs.jre_minimal.override {modules = [ "java.base" "jdk.compiler" ]; };};
+      rocket_config = pkgs.callPackage ./nix/rocket_config.nix {openjdk = pkgs.jre_minimal.override {modules = ["java.base" "jdk.compiler"];};};
 
       container = pkgs.callPackage ./nix/container.nix {inherit wrapper;};
-      container-stream = pkgs.runCommand "container-stream" {script = container.override {stream = true;}; nativeBuildInputs = [pkgs.coreutils];} "mkdir -p $out/bin; ln -s $script $out/bin/container-stream";
+      container-stream = pkgs.runCommand "container-stream" {
+        script = container.override {stream = true;};
+        nativeBuildInputs = [pkgs.coreutils];
+      } "mkdir -p $out/bin; ln -s $script $out/bin/container-stream";
       nixos-vm = (pkgs.nixos [{environment.systemPackages = [default];} ./nix/staging-nixos-config.nix]).vm;
 
       default = wrapper;
@@ -34,7 +38,22 @@
     devShells = let
       shellPackages = pkgs: (with pkgs; [just mprocs rustfmt cargo clippy rust-analyzer]);
     in
-      forAllSystems (system: {default = (packages (pkgsFor system)).backend.overrideAttrs (old: {nativeBuildInputs = old.nativeBuildInputs ++ shellPackages (pkgsFor system);});});
+      forAllSystems (system: {
+        default =
+          (packages (pkgsFor system)).backend.overrideAttrs
+          (
+            old: let
+              pkgs = pkgsFor system;
+            in {
+              # Needed for rust-analyzer to find openssl and libxml2
+              # Also prevents needless rebuilding of their -sys crates
+              OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+              OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+              LIBXML2 = "${pkgs.libxml2.out}/lib/libxml2.so";
+              nativeBuildInputs = old.nativeBuildInputs ++ shellPackages pkgs;
+            }
+          );
+      });
   };
 }
 /*
@@ -51,3 +70,4 @@ docs:
 
 `nix run .#container-stream | docker load
 */
+
